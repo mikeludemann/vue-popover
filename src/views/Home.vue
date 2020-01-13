5647e08af10b9d404d761fe2dfbe86de3d2d5d74
Mike Ludemann <mike-ludemann@hotmail.de>
2020-01-13 15:43:43 +0100

    Setup complete - vue-popover

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+		<popover defaulttext="Hi" popovertext="I am here!" position="bottom"/>
+		<popover defaulttext="Hi" popovertext="I am here!" position="right"/>
+		<popover defaulttext="Hi" popovertext="I am here!" position="left"/>
+		<popover defaulttext="Hi" popovertext="I am here!" position="top"/>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import popover from '@/components/popover.vue'
+
+export default {
+	name: 'home',
+	components: {
+		HelloWorld,
+		popover
+	}
+}
+</script>
